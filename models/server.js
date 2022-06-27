19899e6233cdd0e91d4ebd24d1c8f6a081a1d154
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-27 10:23:18 -0500

    [add] CORS - Middleware

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 class Server {
     constructor() {
@@ -13,7 +14,7 @@ class Server {
     }
 
     middlewares() {
-        // public folder
+        this.app.use(cors());
         this.app.use(express.static('public'));
     }
 
